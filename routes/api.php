a46120f3dd78bdccde2d0059032bb1bbcbbcf068
phongphamcadabra <phong.pham@cadabra.jp>
2023-10-13 16:24:03 +0700

    Phong.Pham 2023/10/13 update api login

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', 'AuthController@login');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('/users', 'UserController@index');
+    // return $request->user();
 });
